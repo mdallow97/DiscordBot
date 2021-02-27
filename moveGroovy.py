81b68f92f75455aef59fa3cf4e898b861ed42e58
mdallow97 <mikedallow@gmail.com>
2021-02-26 17:31:58 -0800

    Removed comment

--- moveGroovy.py
+++ moveGroovy.py
@@ -6,7 +6,6 @@ class MyClient(discord.Client):
 		print('Logged on as {0}!'.format(self.user))
 
 	async def on_message(self, message):
-		# print(message)
 		print('Message from {0.author}: {0.content}'.format(message))
 
 		if len(message.content) > 1 and message.content[0] == '-' and message.content[1] != '-':
